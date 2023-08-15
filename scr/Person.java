a4e94cef275b5184eae5b2e28836d5d555e26243
CapitanDagestan <aleskerovbogdan@gmail.com>
2023-08-15 20:40:48 +0300

    Age check move

--- scr/Person.java
+++ scr/Person.java
@@ -18,11 +18,11 @@ public class Person {
         String town = scanner.nextLine();
         System.out.println("Введіть ваш вік: ");
         int age = scanner.nextInt();
-        System.out.println("Введіть ваш дійсний номер: ");
-        int number = scanner.nextInt();
         if (age < 18) {
             System.out.println("Потрібен дозвіл батьків для реєстрації");
         } else {
+            System.out.println("Введіть ваш дійсний номер: ");
+            int number = scanner.nextInt();
             System.out.println("Нового користувача " + "\"" + name + "\"" + " зареєстровано");
             System.out.println("ID користувача: " + id);
             personInfo(name, town, number);
